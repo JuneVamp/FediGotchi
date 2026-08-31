d5175b0ebb81293242a8dc4d37532df659416697
YashBhartia00 <51514725+JuneVamp@users.noreply.github.com>
2026-09-01 01:09:49 +0900

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url';
 export default defineConfig({
   root: fileURLToPath(new URL('.', import.meta.url)),
   plugins: [react()],
+  base: '/FediGotchi/',
   server: {
     host: '0.0.0.0',
     port: 5173,
